85b73e5599293e2421fbd111e80db919e0e07e1e
Shivaboyz <meiyurshiv11@gmail.com>
2024-06-04 14:00:09 -0400

    code

---
+++ classes/Steganography.java
@@ -0,0 +1,26 @@
+import java.awt.Color;
+public class Steganography {
+
+    public static void clearLow(Pixel p) {
+        p.setColor(new Color(p.getRed()%4, p.getGreen()%4, p.getBlue()%4));
+    }
+
+    public static Picture testClearLow(Picture p) {
+        Picture s = p; 
+        for(int i = 0; i<s.getWidth(); i++) {
+            for(int j = 0; j<s.getHeight(); j++) {
+                clearLow(s.getPixel(i, j));
+            }
+        }
+        return s;
+    }
+
+
+
+    public static void main(String args[]) {
+        Picture beach = new Picture("beach.jpg");
+        beach.explore();
+        Picture copy = testClearLow(beach);
+        copy.explore();
+    } 
+}
